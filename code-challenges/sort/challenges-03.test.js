977cee29294054fd4db9d6ccc2f9279a0fdb58e6
mestock <MelissaStock44@gmail.com>
2019-04-10 21:13:05 -0700

    Passed test8

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -149,7 +149,18 @@ If two people have the same full name, the younger one should come first. Do not
 ------------------------------------------------------------------------------------------------ */
 
 const sortPeopleBetter = (arr) => {
-  // Solution code here...
+  arr.sort( (a,b) => {
+    if(a.lastName > b.lastName){return 1;}
+    if(a.lastName < b.lastName){return -1;}
+    if(a.firstName > b.firstName){return 1;}
+    if(a.firstName < b.firstName){return -1;}
+    if(a.age > b.age){return 1;}
+    if(a.age < b.age){return -1;}
+    return 0;
+    
+  });
+
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -277,7 +288,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should sort people with more strict ordering', () => {
     const family = [
       new Person('Casey', 'Codefellows', 55),

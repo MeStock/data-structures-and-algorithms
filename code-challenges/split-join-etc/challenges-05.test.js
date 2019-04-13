c9ddb4c2002778dcc040c122ce40216f54ae3242
mestock <MelissaStock44@gmail.com>
2019-04-13 16:46:00 -0700

    Passed test6

--- code-challenges/split-join-etc/challenges-05.test.js
+++ code-challenges/split-join-etc/challenges-05.test.js
@@ -162,7 +162,14 @@ For example:
 ------------------------------------------------------------------------------------------------ */
 
 const removeEvenValues = (arr) => {
-  // Solution code here...
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      arr.splice(i, 1);
+      i = i - 1;
+    }
+  }
+
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -273,7 +280,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);

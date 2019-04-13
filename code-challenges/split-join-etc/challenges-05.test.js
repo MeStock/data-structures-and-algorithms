8870b0b384edd990d538242350870f69a1e00db3
mestock <MelissaStock44@gmail.com>
2019-04-13 10:30:04 -0700

    Passed test2

--- code-challenges/split-join-etc/challenges-05.test.js
+++ code-challenges/split-join-etc/challenges-05.test.js
@@ -30,7 +30,14 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  let result = [];
+  let convertToArray = arr.split('');
+
+  for(let i = 0; i < arr.length; i++){
+    let takeFirstElement = convertToArray.shift();
+    result.push(takeFirstElement);
+  }
+  return result;
 };
 
 
@@ -207,7 +214,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array of individual letters', () => {
     expect(wordsToCharList('Gregor')).toStrictEqual(['G', 'r', 'e', 'g', 'o', 'r']);
     expect(wordsToCharList('Gregor').length).toStrictEqual(6);

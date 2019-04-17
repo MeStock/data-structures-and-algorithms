b0ec0450db09c220dca906886a496d7af6388735
mestock <MelissaStock44@gmail.com>
2019-04-16 21:29:05 -0700

    Passed test4

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -57,7 +57,11 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  let result = [];
+  arr.map( char => {
+    result.push(char.charCodeAt());
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -211,7 +215,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);

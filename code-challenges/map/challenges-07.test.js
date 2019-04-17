ae5afdb27fbc32378d5c8c14537b0684afd445f3
mestock <MelissaStock44@gmail.com>
2019-04-16 21:21:41 -0700

    Passed test3

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -39,7 +39,11 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  // Solution code here...
+  let result = [];
+  arr.map( (val) => {
+    result.push(2 ** val)
+  });
+  return result;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -196,7 +200,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(mapTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(mapTwoToThe([0, 4, 5]).length).toStrictEqual(3);

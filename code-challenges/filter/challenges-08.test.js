e2e6e01a03daf7d97d0398d8504adb8c4e3af3cd
mestock <MelissaStock44@gmail.com>
2019-04-17 21:37:06 -0700

    Passed test6

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -164,7 +164,9 @@ const characters = [
 ];
 
 const getCharactersWithoutChildren = (arr) => {
-  // Solution code here...
+  return arr.filter(character => {
+    if(character.children === undefined) return character;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -268,7 +270,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing characters who do not have children', () => {
     expect(getCharactersWithoutChildren(characters)).toStrictEqual([ { name: 'Sansa', spouse: 'Tyrion', house: 'Stark' }, { name: 'Jon', spouse: null, house: 'Snow' } ]);
     expect(getCharactersWithoutChildren(characters).length).toStrictEqual(2);

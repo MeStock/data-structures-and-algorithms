d45e0379418c613b01d4c2a768b25947fd1dfc28
mestock <MelissaStock44@gmail.com>
2019-04-23 23:10:46 -0700

    Passed test4

--- code-challenges/2D-arrays/challenges-12.test.js
+++ code-challenges/2D-arrays/challenges-12.test.js
@@ -105,7 +105,8 @@ The top row of the board is considered row zero and row numbers increase as they
 ------------------------------------------------------------------------------------------------ */
 
 const battleship = (board, row, col) => {
-  //  Solution code here...
+  if(board[row][col] === '#') return 'hit';
+  return 'miss';
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -223,7 +224,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   const battleshipData = [
     ['#', ' ', '#', ' '],
     ['#', ' ', '#', ' '],

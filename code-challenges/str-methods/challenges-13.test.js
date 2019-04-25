ad47d648bff9a293cc16b24db4c13adf2dcc1cc1
mestock <MelissaStock44@gmail.com>
2019-04-24 21:14:03 -0700

    Passed test2

--- code-challenges/str-methods/challenges-13.test.js
+++ code-challenges/str-methods/challenges-13.test.js
@@ -23,7 +23,10 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 ------------------------------------------------------------------------------------------------ */
 
 const findHappiness = (arr) => {
-  // Solution code here...
+  return arr.filter(str => {
+    if(str.includes(':)')) return true;
+    return false;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -156,7 +159,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return only the strings that contain smiley faces', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 

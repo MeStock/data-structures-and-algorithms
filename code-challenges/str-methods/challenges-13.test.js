16cbe4a814ec56d18e7bafae44552c455293d17c
mestock <MelissaStock44@gmail.com>
2019-04-24 21:21:46 -0700

    Passed test3

--- code-challenges/str-methods/challenges-13.test.js
+++ code-challenges/str-methods/challenges-13.test.js
@@ -38,7 +38,12 @@ For example, (123) 456-7890 returns 1234567890
 ------------------------------------------------------------------------------------------------ */
 
 const standardizePhoneNumbers = (arr) => {
-  // Solution code here...
+  return arr.map(str => {
+    return str.split('').filter(eachChar => {
+      if(eachChar === '(' || eachChar === ')' || eachChar === '-' || eachChar === ' ') return false;
+      return true;
+    }).join('');
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -170,7 +175,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return a standardized set of phone numbers', () => {
     const nums = ['(123) 456-7890', '(222) 222-2222'];
 

3237b996f14576f3199c62a73a549f0db2f4f2ae
mestock <MelissaStock44@gmail.com>
2019-05-20 23:38:05 -0700

    Added tests

--- code-challenges/linkedlist/challenges-19.test.js
+++ code-challenges/linkedlist/challenges-19.test.js
@@ -75,3 +75,45 @@ stockFamily.insertBefore('kevin', 'testing');
 
 
 console.log(stockFamily);
+
+
+describe('Linked list', () => {
+  it('shound successfully build an empty linked list', () => {
+    expect(new Node()).toBeInstanceOf(Node);
+  });
+  it('can properly add a node to the list', () => {
+    const test = new Node;
+    expect(test.append('test')).toEqual(expect.objectContaining(test));
+  });
+  xit('should properly insert before the first node of the list', () => {
+    const test = new Node;
+    test.append('head');
+    test.append('first node');
+    test.append('second node');
+    test.insertBefore('head','inserted before node');
+    expect(test.value).toBe('inserted before node');
+  });
+  xit('should properly insert before i in the middle of the list', () => {
+    const test = new Node;
+    test.append('head');
+    test.append('first node');
+    test.append('second node');
+    test.insertBefore('first node','inserted node');
+    expect(test.next.value).toEqual('inserted node');
+  });
+  xit('should properly insert after the last node of the list', () => {
+    const test = new Node;
+    test.append('head');
+    test.append('first node');
+    test.insertAfter('first node', 'inserted node');
+    expect(test.next.next.value).toEqual('inserted node');
+  });
+  xit('should properly insert in the middle of the list', () => {
+    const test = new Node;
+    test.append('head');
+    test.append('first node');
+    test.append('second node');
+    test.insertAfter('first node', 'inserted node');
+    expect(test.next.next.value).toEqual('inserted node');
+  });
+});

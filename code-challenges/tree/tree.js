b040a783784b20cd37497a176694a9db8e305272
mestock <MelissaStock44@gmail.com>
2019-06-01 13:11:37 -0700

    completed node & binary tree class - preorder, inorder, postorder methods

--- code-challenges/tree/tree.js
+++ code-challenges/tree/tree.js
@@ -5,6 +5,8 @@
 Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
 Create a BinaryTree class
 Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
+
+
 At no time should an exception or stack trace be shown to the end user. Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
 
 Create a BinarySearchTree class
@@ -12,3 +14,60 @@ Define a method named add that accepts a value, and adds a new node with that va
 Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 
 */
+
+const Node = require('./node.js');
+
+class BinaryTree{
+  constructor(value){
+    this.root = new Node(value);
+  }
+  preOrder(node = this.root, values = []){
+    if(node){
+      values.push(node.value);
+      if(node.left !== null){
+        this.preOrder(node.left, values);
+      }
+      if(node.right !== null){
+        this.preOrder(node.right, values);
+      }
+    }
+    return values;
+  }
+
+  inOrder(node = this.root, values = []){
+    if(node){
+      if(node.left !== null){
+        this.inOrder(node.left, values);
+      }
+      values.push(node.value);
+      if(node.right !== null){
+        this.inOrder(node.right, values);
+      }
+    }
+    return values;
+  }
+
+  postOrder(node = this.root, values = []){
+    if(node){
+      if(node.left !== null){
+        this.postOrder(node.left, values);
+      }
+      if(node.right !== null){
+        this.postOrder(node.right, values);
+      }
+      values.push(node.value);
+    }
+    return values;
+  }
+}
+
+const test = new BinaryTree('root');
+test.root.left = new Node('left');
+test.root.right = new Node('right');
+
+console.log(test.preOrder());
+console.log(test.inOrder());
+console.log(test.postOrder());
+
+
+module.exports = BinaryTree;

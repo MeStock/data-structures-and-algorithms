18f4fee652929bd0751db1f7c5c067fe567f51fb
mestock <MelissaStock44@gmail.com>
2019-05-31 22:37:10 -0700

    binary search tree

---
+++ __tests__/tree.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*
+
+Can successfully instantiate an empty tree
+Can successfully instantiate a tree with a single root node
+Can successfully add a left child and right child to a single root node
+Can successfully return a collection from a preorder traversal
+Can successfully return a collection from an inorder traversal
+Can successfully return a collection from a postorder traversal
+
+*/
---
+++ code-challenges/tree/tree.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+
+Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
+Create a BinaryTree class
+Define a method for each of the depth first traversals called preOrder, inOrder, and postOrder which returns an array of the values, ordered appropriately.
+At no time should an exception or stack trace be shown to the end user. Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
+
+Create a BinarySearchTree class
+Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
+
+*/

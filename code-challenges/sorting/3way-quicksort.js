8d8e2d8a8c2e12b0cc1b44c21fd28c0615b10846
Melissa Stock <melissastock44@gmail.com>
2019-07-01 15:41:24 -0700

    Added some comments to 3way partition

--- code-challenges/sorting/3way-quicksort.js
+++ code-challenges/sorting/3way-quicksort.js
@@ -11,8 +11,8 @@ const _swap = (items, indexA, indexB) => {
 
 const _partition = (items, leftIndex, rightIndex) => {
   const pivotIndex = rightIndex;
-  let firstEqual = rightIndex;
-  let lastEqual = rightIndex;
+  let firstEqual = rightIndex; //set the first equal to the pivot to start
+  let lastEqual = rightIndex; //set the last equal to the pivot to start
   let firstHighIndex = leftIndex;
 
   for(let i = leftIndex; i < rightIndex; i ++){
@@ -21,22 +21,23 @@ const _partition = (items, leftIndex, rightIndex) => {
       firstHighIndex++;
     }
     if(items[i] === items[pivotIndex]){
-      _swap(items, i, firstEqual - 1);
-      firstEqual = firstEqual - 1;
+      _swap(items, i, firstEqual - 1); //move duplicate values next to the pivot
+      firstEqual = firstEqual - 1; //set the first equal to the duplicate
       if(items[i] < items[pivotIndex]) {
-        _swap(items, i, firstHighIndex);
+        _swap(items, i, firstHighIndex); //do the same firstHigh check now that the duplicate is removed
         firstHighIndex++;
       }
     }
   }
   _swap(items, pivotIndex, firstHighIndex);
-  return [firstEqual, lastEqual];
+  return [firstEqual, lastEqual]; //return the range where all the duplicates exist (next to the pivot)
 };
 
 const _helper = (items, leftIndex, rightIndex) => {
   if (rightIndex > leftIndex) {
     const partitionIndex = _partition(items, leftIndex, rightIndex);
 
+    //when we get here - we use recursion but remove the entire section of duplicate values
     _helper(items,leftIndex, partitionIndex[0] -1);
     _helper(items, partitionIndex[1] + 1, rightIndex);
   }

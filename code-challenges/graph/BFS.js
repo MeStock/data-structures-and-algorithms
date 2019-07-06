3b3ef2b8cb943b7510734f73065e77193b1a3eb8
Melissa Stock <melissastock44@gmail.com>
2019-07-06 10:42:23 -0700

    Changed arrays in bfs.js to actual queues

--- code-challenges/graph/BFS.js
+++ code-challenges/graph/BFS.js
@@ -1,32 +1,33 @@
 'use strict';
+const Queue = require('queue-fifo');
 
 module.exports = (graph, startVertex, goalVertex) => {
-  const queue = []; // Vinicio - HW, make this a real queue - https://www.npmjs.com/package/queue-fifo
+  const queue = new Queue(); // Vinicio - HW, make this a real queue - https://www.npmjs.com/package/queue-fifo
   const visitedVertices = new Set();
 
   const paths = new Map();
 
-  queue.unshift(startVertex);
+  queue.enqueue(startVertex);
   visitedVertices.add(startVertex);
 
 
-  while(queue.length > 0) {
+  while(queue.size() > 0) {
     // dequeue
     // check for goal
     // loop over all the neighobrs
-    const currentVertex = queue.pop();
+    const currentVertex = queue.dequeue();
 
     if(currentVertex === goalVertex) {
       return paths;
     }
-    
+
     let neighbor = graph.getNeighbors(currentVertex);
     while(neighbor){
       if(!visitedVertices.has(neighbor.value.vertex)){
         visitedVertices.add(neighbor.value.vertex);
       }
       paths.set(neighbor.value.vertex, currentVertex);
-      queue.unshift(neighbor.value.vertex);
+      queue.enqueue(neighbor.value.vertex);
       neighbor = neighbor.next;
     }
   }
